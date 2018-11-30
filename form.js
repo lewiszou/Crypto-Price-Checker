f2b1fcfd0bfcc776a1e123062bacf86d6948c6ef
lewiszou <43123266+lewiszou@users.noreply.github.com>
2018-11-29 23:39:39 -0500

    edit

--- form.js
+++ form.js
@@ -1,14 +1,16 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbz-sJ6q2H2SLhLSo06up9QTu62oeM90YQN-7nOLPauNSYxVPRuM/exec'
+url = 'https://script.google.com/macros/s/AKfycbz-sJ6q2H2SLhLSo06up9QTu62oeM90YQN-7nOLPauNSYxVPRuM/exec'
 
 $('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-  );
+e.preventDefault();
+var jqxhr = $.ajax({
+url: url,
+method: "GET",
+dataType: "json",
+data: $form.serializeObject()
+}).success(
+// do something
+);
 })
+
+var script_url = 'https://script.google.com/macros/s/AKfycbz-sJ6q2H2SLhLSo06up9QTu62oeM90YQN-7nOLPauNSYxVPRuM/exec'
---
+++ note.js
@@ -0,0 +1,23 @@
+/* Example in Node.js ES6 using request-promise, concepts should translate to your language of choice */
+  
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    start: 1,
+    limit: 5000,
+    convert: 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': '8039c519-ce4d-4b17-8103-7e02d335275e'
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  console.log('API call response:', response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
