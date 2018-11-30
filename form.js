5f0359d32c4ddac11d1cbf731e1a16ab18a7a5fc
lewiszou <43123266+lewiszou@users.noreply.github.com>
2018-11-29 22:18:28 -0500

    edits

---
+++ form.js
@@ -0,0 +1,14 @@
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbz-sJ6q2H2SLhLSo06up9QTu62oeM90YQN-7nOLPauNSYxVPRuM/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
